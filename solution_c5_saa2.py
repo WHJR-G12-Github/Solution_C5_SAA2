8af3537a5006b3e94711aafd3f969341969a9270
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-19 10:34:15 +0530

    Create solution_c5_saa2.py

---
+++ solution_c5_saa2.py
@@ -0,0 +1,11 @@
+# Creating a list 'names' to store names of 5 friends
+names = ["Ram","Ved","Dev","Dhruv","Geet"]
+
+# Creating a list 'hobbies' to store hobbies of 5 friends
+hobbies=["Reading","Playing football","Playing chess","Gardening","Singing"]
+
+
+# Creating a "for" loop to repeat 5 times
+for i in range(5):
+  # Printing the friend's name and the corresponding hobby
+  print(names[i],hobbies[i])
